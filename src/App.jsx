0a81bbc1f4315d839a83ff3b83e6e1533488e650
Abhijith M R <abhijithmr581@gmail.com>
2024-01-04 18:33:00 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Blog from "./components/Blog.jsx";
+import Contact from "./components/Contact.jsx";
+import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+import Projects from "./components/Project.jsx";
+import About from "./components/about.jsx";
+
+
+
+
+function App() {
+  return(
+  <>
+  <Header />
+  <Hero />
+  <About />
+  <Blog />
+  <Projects />
+  <Contact />
+  </>
+  )
+}
+
+export default App
